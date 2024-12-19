ca36c544affcbbc0f50007c41123bddf3c2df645
Senne Cools <113857183+sennecools@users.noreply.github.com>
2024-12-19 22:58:51 +0100

    Added create board to services

--- front-end/services/BoardService.ts
+++ front-end/services/BoardService.ts
@@ -1,5 +1,34 @@
 import { getToken, getUserId } from './LocalStorageService';
 
+const createBoard = async (name: string) => {
+    const token = getToken();
+    const userId = getUserId();
+    if (!token) {
+        throw new Error('No authentication token found.');
+    }
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/boards`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ name, userId }),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.text();
+            throw new Error(errorData || 'Failed to create board');
+        }
+
+        return response.json();
+    } catch (error) {
+        console.error('Error creating board:', error);
+        throw new Error('Unable to connect to the server or invalid input.');
+    }
+};
+
 const getAllBoards = async (): Promise<any> => {
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/boards`, {
@@ -24,6 +53,66 @@ const getAllBoards = async (): Promise<any> => {
         }
     } catch (error) {
         console.error('Error fetching boards:', error);
+        throw new Error('Unable to connect to the server or invalid input.');
+    }
+};
+
+const getBoardByUserId = async (): Promise<any> => {
+    const token = getToken();
+    const userId = getUserId();
+    if (!token) {
+        throw new Error('No authentication token found.');
+    }
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/boards/user/${userId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            const errorData = await response.text();
+            throw new Error(errorData || 'Failed to fetch boards');
+        }
+
+        const contentType = response.headers.get('Content-Type');
+
+        if (contentType && contentType.includes('application/json')) {
+            const data = await response.json();
+            return data;
+        } else {
+            throw new Error('Expected JSON, but received: ' + contentType);
+        }
+    } catch (error) {
+        console.error('Error fetching boards:', error);
+        throw new Error('Unable to connect to the server or invalid input.');
+    }
+};
+
+const removeBoardFromUser = async (boardId: number) => {
+    const token = getToken();
+    if (!token) {
+        throw new Error('No authentication token found.');
+    }
+
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/boards/${boardId}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            const errorData = await response.text();
+            throw new Error(errorData || 'Failed to delete board');
+        }
+
+        return response.json();
+    } catch (error) {
+        console.error('Error deleting board:', error);
         throw new Error('Unable to connect to the server or invalid input.');
     }
 };
@@ -81,48 +170,12 @@ const updatePinBoards = async (pinId: number, boardIds: number[]) => {
     }
 };
 
-const getUserBoards = async (): Promise<any> => {
-    try {
-        const userId = getUserId();
-        if (!userId) {
-            throw new Error('No user ID found');
-        }
-
-        const token = getToken();
-        if (!token) {
-            throw new Error('No authentication token found.');
-        }
-
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/boards/user/${userId}`, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-
-        if (!response.ok) {
-            const errorData = await response.text();
-            throw new Error(errorData || 'Failed to fetch boards');
-        }
-
-        const contentType = response.headers.get('Content-Type');
-
-        if (contentType && contentType.includes('application/json')) {
-            const data = await response.json();
-            return data;
-        } else {
-            throw new Error('Expected JSON, but received: ' + contentType);
-        }
-    } catch (error) {
-        console.error('Error fetching boards:', error);
-        throw new Error('Unable to connect to the server or invalid input.');
-    }
-};
-
 const BoardService = {
+    createBoard,
     getAllBoards,
     getBoardById,
-    getUserBoards,
+    getBoardByUserId,
+    removeBoardFromUser,
     updatePinBoards,
 };
 
